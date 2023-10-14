bfc45619e5d379882261e56c5c093c9fbbb92c1b
Kiran <smkiran.09@gmail.com>
2023-10-14 23:30:59 +0530

    day 3

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
-    <div>
-      Hello
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">$ {props.amount}</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
